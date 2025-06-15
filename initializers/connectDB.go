4080700233be96f5bfe5d0cf2cd06cdbf32d24af
Vineet Salve <vineetsalve23@gmail.com>
2025-06-16 00:43:00 +0530

    Add DB_PASSWORD support for Railway deployment

--- initializers/connectDB.go
+++ initializers/connectDB.go
@@ -12,7 +12,10 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	dsn := fmt.Sprintf("host=%s user=%s dbname=%s port=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_USER"),
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Kolkata",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
 		os.Getenv("DB_PORT"),
 	)
